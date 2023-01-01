7a6d42a5b1fef61b7ba8ecdbe9b12c73194c9dfb
ApuEmdad <emdadul.apu@savethechildren.org>
2023-01-01 20:40:40 +0600

    counter function added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const handleClick = () => {
+    setCount(count + 1);
+  };
   return (
     <div className="App">
       <h1>Hello world</h1>
+      <button onClick={handleClick}>click me</button>
+      <h3>Count: {count}</h3>
     </div>
   );
 }
